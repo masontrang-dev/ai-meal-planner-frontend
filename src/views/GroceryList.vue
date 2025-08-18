4a6ca05a724981b5cc56f34f9437b3f2d2d3ad8c
Mason Trang <mason.trang@gmail.com>
2025-08-18 14:33:44 -0700

    initial setup of grocery list

--- src/views/GroceryList.vue
+++ src/views/GroceryList.vue
@@ -1,7 +1,73 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import OrderList from "primevue/orderlist";
+import { ref, watch } from "vue";
+
+const groceries = ref([
+  { id: 1, name: "Apples", checked: false },
+  { id: 2, name: "Bananas", checked: false },
+  { id: 3, name: "Carrots", checked: false },
+  { id: 4, name: "Doughnuts", checked: false },
+]);
+
+const selectedItems = ref([]);
+
+watch(selectedItems, (newSelection) => {
+  groceries.value.forEach((g) => {
+    g.checked = newSelection.some((item) => item.id === g.id);
+  });
+});
+
+// Handler for checkbox click
+function onCheck(option) {
+  option.checked = !option.checked;
+  if (option.checked) {
+    // Add to selectedItems if not already present
+    if (!selectedItems.value.some((item) => item.id === option.id)) {
+      selectedItems.value = [...selectedItems.value, option];
+    }
+  } else {
+    // Remove from selectedItems
+    selectedItems.value = selectedItems.value.filter(
+      (item) => item.id !== option.id
+    );
+  }
+}
+</script>
 
 <template>
-  <div class="home">Grocery List</div>
+  <!-- <OrderList
+    v-model="groceries"
+    dataKey="id"
+    breakpoint="575px"
+    pt:pcListbox:root="w-full sm:w-56"
+    selectionMode="single"
+    :selection="selectedItems"
+  >
+    <template #option="{ option }">
+      <span>
+        <input
+          type="checkbox"
+          :checked="option.checked"
+          @change="onCheck(option)"
+          style="margin-right: 8px"
+        />
+        {{ option.name }}
+      </span>
+    </template>
+  </OrderList> -->
+  <div
+    v-for="option in groceries"
+    :key="option.id"
+    class="flex flex-col items-start gap-2"
+  >
+    <span>
+      <input
+        type="checkbox"
+        :checked="option.checked"
+        @change="onCheck(option)"
+        style="margin-right: 8px"
+      />
+      {{ option.name }}
+    </span>
+  </div>
 </template>
-
-<style scoped></style>
